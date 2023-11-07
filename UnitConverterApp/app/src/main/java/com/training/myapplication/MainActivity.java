751770a7a7a12c3ef2c1fd8a0dbcba97ab107b04
NizamiQarayevWork <nizamiq@maestro.st>
2023-11-07 16:11:44 +0400

    unit converter app done

--- UnitConverterApp/app/src/main/java/com/training/myapplication/MainActivity.java
+++ UnitConverterApp/app/src/main/java/com/training/myapplication/MainActivity.java
@@ -3,15 +3,46 @@ package com.training.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText numberInput;
+    Button converterButton;
+    TextView outputText;
+
+
+    public double conversion(double convertableKiloUnit){
+    return convertableKiloUnit*2.20462;
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        numberInput= findViewById(R.id.editTextNumber);
+        converterButton= findViewById(R.id.button);
+        outputText=findViewById((R.id.textView));
 
 
-        Number
+
+
+        converterButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+              double inputValue= Double.parseDouble(numberInput.getText().toString())  ;
+                double finalValue=conversion(inputValue);
+                outputText.setText(""+finalValue+" pounds");
+            }
+        });
+
+
+
     }
 }
